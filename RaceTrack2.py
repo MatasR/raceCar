8f803bddb8d6e03d3365602ef344a50efad09fb9
MatasR <batassp@gmail.com>
2020-01-17 20:00:40 +0200

    little code beauty fix

--- RaceTrack2.py
+++ RaceTrack2.py
@@ -42,7 +42,6 @@ class RaceTrack:
         pygame.draw.polygon(screen, config.C_GREEN, innerPoints)
 
         # OUTER HEART
-        # Right side of the heart
         i = 0
         for x in outerPoints:
             pygame.draw.line(screen, config.C_YELLOW, outerPoints[i], outerPoints[i+1], 3)
@@ -50,6 +49,7 @@ class RaceTrack:
             # If last point - break the loop.
             if i+1 == len(outerPoints):
                 break
+        # Right side of the heart
         '''pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth/2, config.screenHeight), (config.screenWidth - 10, 200), 3)
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth - 10, 200), (config.screenWidth - 10, 100), 3)
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth - 10, 100), (config.screenWidth - 100, 10), 3)
@@ -57,13 +57,6 @@ class RaceTrack:
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2 + 200, 10), (config.screenWidth / 2, 200), 3)'''
 
         # Left side of the heart
-        i = 0
-        for x in innerPoints:
-            pygame.draw.line(screen, config.C_YELLOW, innerPoints[i], innerPoints[i + 1], 3)
-            i += 1
-            # If last point - break the loop.
-            if i + 1 == len(innerPoints):
-                break
         '''pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2, 200), (config.screenWidth / 2 - 200, 10), 3)
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2 - 200, 10), (100, 10), 3)
         pygame.draw.line(screen, config.C_YELLOW, (100, 10), (10, 100), 3)
@@ -71,14 +64,21 @@ class RaceTrack:
         pygame.draw.line(screen, config.C_YELLOW, (10, 200), (config.screenWidth/2, config.screenHeight), 3)'''
 
         # INNER HEART
+        i = 0
+        for x in innerPoints:
+            pygame.draw.line(screen, config.C_YELLOW, innerPoints[i], innerPoints[i + 1], 3)
+            i += 1
+            # If last point - break the loop.
+            if i + 1 == len(innerPoints):
+                break
         # Right side of the heart
-        pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2, config.screenHeight - 100), (config.screenWidth - 110, 175), 3)
+        '''pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2, config.screenHeight - 100), (config.screenWidth - 110, 175), 3)
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth - 110, 175), (config.screenWidth - 110, 110), 3)
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth - 110, 110), (config.screenWidth / 2 + 250, 110), 3)
-        pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2 + 250, 110), (config.screenWidth / 2, 300), 3)
+        pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2 + 250, 110), (config.screenWidth / 2, 300), 3)'''
 
         # Left side of the heart
-        pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2, 300), (config.screenWidth / 2 - 250, 110), 3)
+        '''pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2, 300), (config.screenWidth / 2 - 250, 110), 3)
         pygame.draw.line(screen, config.C_YELLOW, (config.screenWidth / 2 - 250, 110), (110, 110), 3)
         pygame.draw.line(screen, config.C_YELLOW, (110, 110), (110, 175), 3)
-        pygame.draw.line(screen, config.C_YELLOW, (110, 175), (config.screenWidth / 2, config.screenHeight - 100), 3)
+        pygame.draw.line(screen, config.C_YELLOW, (110, 175), (config.screenWidth / 2, config.screenHeight - 100), 3)'''
